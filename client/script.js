752a5918af58a7c180ad3cf508cea2bedeb5b295
Saskia <saskiarief@yahoo.de>
2023-04-16 18:50:47 +0200

    add alert in case of wrong password

--- client/script.js
+++ client/script.js
@@ -2,6 +2,8 @@ const pointsUser0 = document.getElementById("user-points-0")
 const pointsUser1 = document.getElementById("user-points-1")
 const form = document.getElementById("form")
 const resetButton = document.getElementById("reset-button")
+const close = document.getElementById("close-button")
+const pwAlert = document.getElementById("pwAlert")
 
 if(getCookie('password') == null) {
     document.cookie = 'password' + '=' + prompt("Passwort eingeben:")
@@ -23,10 +25,15 @@ form.addEventListener("submit", (e) => {
     e.preventDefault()
     const formData = new FormData(form)
     const data = Object.fromEntries(formData)
+    pwAlert.style.display = "none"
     fetch("/api/user_points", {
         method: "post",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({user: data.user, points: data.points, password: getCookie('password')})
+    }).then((res) => {
+        if(res.status === 403) {
+            pwAlert.style.display = "block"
+        }
     })
     fetch("/api/user_points").then((res) => {
         res.json().then((data) => {
@@ -40,12 +47,15 @@ resetButton.addEventListener("click", (e) => {
     document.cookie = 'password' + '=' + prompt("Passwort eingeben:")
 })
 
+close.addEventListener("click", (e) => {
+    e.preventDefault()
+    pwAlert.style.display = "none"
+})
+
 fetch("/api/user_points").then((res) => {
     res.json().then((data) => {
         setPoints(data.points)
     })
 })
 
-function showError() {
 
-}
